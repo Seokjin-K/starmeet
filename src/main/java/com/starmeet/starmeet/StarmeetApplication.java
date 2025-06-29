20639d266e548487375372f8bfb0adcf0de9d7ed
seokjin <wlsdldos00@gmail.com>
2025-06-29 12:12:26 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/com/starmeet/starmeet/StarmeetApplication.java
@@ -0,0 +1,13 @@
+package com.starmeet.starmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarmeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarmeetApplication.class, args);
+	}
+
+}
